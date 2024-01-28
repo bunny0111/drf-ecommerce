acddf6aae56b671c64b793f4815ddfb58a45dec3
NETLING <netling@pop-os.localdomain>
2024-01-29 00:30:42 +0530

    Complete Phase 1 Code

--- drfEcommerce/drfEcommerce/product/views.py
+++ drfEcommerce/drfEcommerce/product/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+        '''
+            A simple viewset for viewing categories.
+        '''
+
+        queryset = Category.objects.all()
+
+# Decorator:- A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure. They are an excellent way to apply reusable functionality across multiple functions, such as timing, caching, logging, or authentication.
+
+        @extend_schema(responses = CategorySerializer)
+        def list(self, request):
+                serializer = CategorySerializer(self.queryset, many=True)
+                return Response(serializer.data)
+        
+class BrandViewSet(viewsets.ViewSet):
+        '''
+            A simple viewset for viewing brands.
+        '''
+
+        queryset = Brand.objects.all()
+
+        @extend_schema(responses = BrandSerializer)
+        def list(self, request):
+                serializer = BrandSerializer(self.queryset, many=True)
+                return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ViewSet):
+        '''
+            A simple viewset for viewing products.
+        '''
+
+        queryset = Product.objects.all()
+
+        @extend_schema(responses = ProductSerializer)
+        def list(self, request):
+                serializer = ProductSerializer(self.queryset, many=True)
+                return Response(serializer.data)
--- drfEcommerce/drfEcommerce/settings/base.py
+++ drfEcommerce/drfEcommerce/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External packages
     'rest_framework',
+    'drf_spectacular',
     # Internal Apps
     'drfEcommerce.product',
 ]
@@ -111,4 +112,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Ecommerce',
+}
